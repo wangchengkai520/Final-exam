20d4d924a575fa553e606057ecf82b2276206d2d
wangchengkai520 <56427726+wangchengkai520@users.noreply.github.com>
2020-01-06 23:50:12 -0800

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,10 +1,9 @@
-
 #include<stdio.h>
 int main(){
     double x;
     double y;
     double z;
-    printf("please input two numbers(divided by space)\n");
+    printf("输入两个数(这两个数用空格隔开):\n");
     scanf("%lf %lf",&x,&y);
     z=(x+y)*(x+y);
     printf("%.2lf\n",z);
